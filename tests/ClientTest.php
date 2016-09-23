be81e2e54c564704e30718bae5d1290b5af51863
alexandre <alexandre.leilber@gmail.com>
2016-09-23 10:15:35 -0400

    add new test for save and rename tests with test_ so that they are found by phpunit

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -20,17 +20,38 @@
             Stylist::deleteAll();
         }
 
-        function getId()
+        function test_getId()
         {
             //ARRANGE
             $id = 1;
             $stylist_id = 2;
-            $name = 'Papa Smurf';
-            $new_client = new Client($name, $stylist_id, $id);
+            $client_name = "Papa Smurf";
+            $new_client = new Client($client_name, $stylist_id, $id);
             $expected_output = 1;
 
             //ACT
             $result = $new_client->getId();
+
+            //ASSERT
+            $this->assertEquals($expected_output, $result);
+        }
+
+        function test_save()
+        {
+            //ARRANGE
+            $stylist_name = "Neymar";
+            $new_stylist = new Stylist($stylist_name);
+            $new_stylist->save();
+            $new_stylist_id = $new_stylist->getId();
+
+            $client_name = "Rasputin Nitupsar";
+            $new_client = new Client($client_name, $new_stylist_id);
+
+            //ACT
+            $new_client->save();
+            $expected_output = $new_client;
+            $all_clients = Client::getAll();
+            $result = $all_clients[0];
 
             //ASSERT
             $this->assertEquals($expected_output, $result);
