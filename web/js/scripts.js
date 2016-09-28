234d5b4ac8e1e69835392077462091f3e923c339
Alexandre <alexandre.leibler@gmail.com>
2016-09-28 17:53:36 -0400

    dropdown displays name of chosen stylist

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -3,8 +3,11 @@ $(document).ready(function() {
     $(".stylist-dropdown-list li").click(function(){
         //get value from list element
         var stylistId = $(this).attr("value");
+        var stylistName = $(this).attr("id");
         //put the value into the hidden input
         $("input[name='stylist_id']").val(stylistId);
+        //display the value as the dropdown button text
+        $("#stylist-dropdown .dropdown-button-text p").text(stylistName);
     })
 
 });
